c9531b847d337f320eeb20cdf954598f39c16ae5
Purinda Gunasekara <purinda@gmail.com>
2015-01-24 08:04:39 +1100

    Improvements to handle arguments better.

--- src/Nerdstorm/FileMime/MimeHelper.php
+++ src/Nerdstorm/FileMime/MimeHelper.php
@@ -8,8 +8,18 @@ class MimeHelper
 {
     static $finfo = null;
 
+    /**
+     * Get MIME-type from a file in the file system.
+     *
+     * @param  string $filepath
+     * @return array
+     */
     public static function guessMimetype($filepath)
     {
+        if (!file_exists($filepath)) {
+            throw new Exception('File doesn\'t exist ' . $filepath);
+        }
+
         if (null === self::$finfo) {
             self::$finfo = finfo_open(FILEINFO_MIME_TYPE);
         }
@@ -17,10 +27,34 @@ class MimeHelper
         return finfo_file(self::$finfo, $filepath);
     }
 
-    public static function guessExtensionFilePath($filepath)
+    /**
+     * Get the file extension from the file in the filesystem.
+     * If there are more than one matching extension,
+     * returning array will have each extension.
+     *
+     * @param  string $filepath
+     * @return array
+     */
+    public static function guessExtensionFromFilePath($filepath)
     {
         $mime_type = self::guessMimetype($filepath);
 
+        if (empty($mime_type)) {
+            throw new UnexpectedValueException('empty MIME-type');
+        }
+
+        return MimeTypes::getExtension($mime_type);
+    }
+
+    /**
+     * Get the file extension from the MIME-type. If there are more than
+     * one matching extension, returning array will have each extension.
+     *
+     * @param  string $mimetype
+     * @return array
+     */
+    public static function guessExtensionFromMimeType($mimetype)
+    {
         if (empty($mime_type)) {
             throw new UnexpectedValueException('empty MIME-type');
         }
